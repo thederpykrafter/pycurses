189f0eeb337fb4c4df4f860e403c97df8888ccf2
thederpykrafter <thederpykrafter@gmail.com>
2025-06-25 19:29:21 -0400

    working and print key pressed

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python
 
-from curses import wrapper
-import curses
 from time import sleep
+import curses
 
 
-def seperator(scr, row):
+def seperator(screen, row):
     for i in range(curses.COLS):
         if i > 0 and i < curses.COLS - 1:
-            scr.addstr(row, i, "{}".format("x"))
+            screen.addstr(row, i, "{}".format("x"))
 
 
 def main(stdscr):
     should_exit = False
 
-    stdscr.clear()
-    stdscr.border()
-    curses.curs_set(0)
-
-    stdscr.addstr(1, 1, "Size: {}x{}".format(curses.LINES, curses.COLS))
-    seperator(stdscr, 2)
-
-    kb_input = stdscr.getkey()
-
     while should_exit == False:
+        stdscr.clear()
+        stdscr.border()
+        curses.curs_set(0)
+
+        stdscr.addstr(1, 1, "Size: {}x{}".format(curses.LINES, curses.COLS))
+        seperator(stdscr, 2)
+
+        kb_input = stdscr.getkey()
+
         match kb_input:
             case "q":
                 should_exit = True
             case _:
                 should_exit = False
 
-        stdscr.addstr(curses.LINES - 1, 1, kb_input)
+        keydisplay = "[" + kb_input + "]"
+        stdscr.addstr(curses.LINES - 1, 1, keydisplay)
         stdscr.refresh()
-        sleep(2)
+        sleep(0.5)
 
 
-wrapper(main)
+curses.wrapper(main)
